d34d450819d56ca31beeda388b4da03a627e403d
GAierken <g.aierken@gmail.com>
2020-04-06 23:36:56 -0400

    added <link> semantic css to html to show icons

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import './App.css';
+import {  Header } from 'semantic-ui-react'
+import Contacts from './Components/Contacts'
+
 
 function App() {
   return (
-    <div className="App">
-      app
-    </div>
+    <React.Fragment>
+      <Header as='h1'>Guligena Aierken</Header>
+      <Contacts/>
+      
+    </React.Fragment>
   );
 }
 
---
+++ portfolio/src/Components/Contacts.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Icon } from 'semantic-ui-react'
+
+ const Contacts = () => {
+    return (
+        <div>
+            <Icon name='linkedin'/>
+            <Icon name='github square'/>
+            <Icon name='medium'/>
+            <Icon name='mail'/>
+            <Icon name='file alternate'/>
+        </div>
+        
+    )
+}
+
+export default Contacts
