ca1d880be296bc883953445c223db0e8ae2a6564
GAierken <g.aierken@gmail.com>
2020-04-07 22:14:54 -0400

    deleted menu

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -4,13 +4,14 @@ import {  Header } from 'semantic-ui-react'
 import Contacts from './Components/Contacts'
 
 
+
+
 function App() {
   return (
     <React.Fragment>
       <Contacts/>
-      <Header textAlign='center' as='h1'>Guligena Aierken</Header>
-      <Header textAlign='center' as='h3'>Full Stack Developer | Software Engineer</Header>
-      
+      <Header textAlign='center' className="home my name">Guligena Aierken</Header>
+      <Header textAlign='center' className="home my job title ">Full Stack Developer | Software Engineer</Header>
       
     </React.Fragment>
   );
