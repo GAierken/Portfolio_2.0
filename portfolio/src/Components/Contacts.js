a3c6c01599036967ca69af4b86a57c9c6add6d42
GAierken <g.aierken@gmail.com>
2020-05-04 23:00:18 -0400

    resized icons in contacts

--- portfolio/src/Components/Contacts.js
+++ portfolio/src/Components/Contacts.js
@@ -11,12 +11,12 @@ import Resume from '../resume.pdf'
 
     
     return (
-        <div>
-            <Popup style={popupStyle} inverted size='mini' content='LinkedIn' trigger={<Icon style={{top: '500px'}} color="purple" link size="huge" name='linkedin' onClick={() => {window.open('https://www.linkedin.com/in/guligena-aierken/')}}/>}/>
-            <Popup style={popupStyle} inverted size='mini' content='GitHub' trigger={<Icon color='purple' link size="huge" name='github square' onClick={() => {window.open('https://github.com/GAierken')}}/>}/>
-            <Popup style={popupStyle} inverted size='mini' content='Medium' trigger={<Icon color='purple' link size="huge" name='medium' onClick={() => {window.open('https://medium.com/@g.aierken')}}/>}/>
-            <Popup style={popupStyle} inverted size='mini' content='Email Me' trigger={ <Icon color='purple' link size="huge" name='mail' onClick={() => {window.open('mailto:g.aierken@gmail.com')}}/>}/>
-            <Popup style={popupStyle} inverted size='mini' content='Résumé' trigger={<Icon color='purple' link size="huge" name='file alternate' onClick={() => {window.open(Resume)}}/>}/>
+        <div style={{marginTop: '50px', marginBottom: '50px'}}>
+            <Popup style={popupStyle} inverted size='mini' content='LinkedIn' trigger={<Icon style={{top: '500px'}} color="purple" link size="massive" name='linkedin' onClick={() => {window.open('https://www.linkedin.com/in/guligena-aierken/')}}/>}/>
+            <Popup style={popupStyle} inverted size='mini' content='GitHub' trigger={<Icon color='purple' link size="massive" name='github square' onClick={() => {window.open('https://github.com/GAierken')}}/>}/>
+            <Popup style={popupStyle} inverted size='mini' content='Medium' trigger={<Icon color='purple' link size="massive" name='medium' onClick={() => {window.open('https://medium.com/@g.aierken')}}/>}/>
+            <Popup style={popupStyle} inverted size='mini' content='Email Me' trigger={ <Icon color='purple' link size="massive" name='mail' onClick={() => {window.open('mailto:g.aierken@gmail.com')}}/>}/>
+            <Popup style={popupStyle} inverted size='mini' content='Résumé' trigger={<Icon color='purple' link size="massive" name='file alternate' onClick={() => {window.open(Resume)}}/>}/>
             
             
         </div>
