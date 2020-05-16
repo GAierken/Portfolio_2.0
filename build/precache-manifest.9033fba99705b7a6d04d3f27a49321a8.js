7890c939471b28d2782e5c6f7d98b415c13d15cc
GAierken <g.aierken@gmail.com>
2020-05-15 21:39:52 -0400

    deleted portfolio folder

---
+++ build/precache-manifest.9033fba99705b7a6d04d3f27a49321a8.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64823e9c72fd6cc3728bf17a169dab80",
+    "url": "/index.html"
+  },
+  {
+    "revision": "99d6e0458f5ea8f5828b",
+    "url": "/static/css/main.f3d7b830.chunk.css"
+  },
+  {
+    "revision": "a0d2b97bb90540aeccbf",
+    "url": "/static/js/2.01dddb24.chunk.js"
+  },
+  {
+    "revision": "2089965cc3e0fbb1dda2d650d8277df4",
+    "url": "/static/js/2.01dddb24.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99d6e0458f5ea8f5828b",
+    "url": "/static/js/main.0c2f7ba9.chunk.js"
+  },
+  {
+    "revision": "4c15ceb5499f6abee85b",
+    "url": "/static/js/runtime-main.eb07c764.js"
+  },
+  {
+    "revision": "0ce03c0b0e62d8e28a53ab2728a33102",
+    "url": "/static/media/nyc.0ce03c0b.png"
+  },
+  {
+    "revision": "6e3230be35864aec80e5bf26d108f186",
+    "url": "/static/media/resume.6e3230be.pdf"
+  },
+  {
+    "revision": "0abc4b7bcab8ad934731006f877c096f",
+    "url": "/static/media/shop.0abc4b7b.png"
+  },
+  {
+    "revision": "efd632fc965a9943c3beab16eda8ca0e",
+    "url": "/static/media/therappoint.efd632fc.png"
+  }
+]);
