81894c11bac5ae120f144c53eb70e5b3ff3681fb
GAierken <g.aierken@gmail.com>
2020-05-15 22:53:09 -0400

    ran npm run build

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9033fba99705b7a6d04d3f27a49321a8.js"
+  "/precache-manifest.77e7d8485b9196bbc92ae6ffc728984f.js"
 );
 
 self.addEventListener('message', (event) => {
