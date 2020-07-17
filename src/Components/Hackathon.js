bc8a82735aad22eb1f9ec171908422b6efc33277
GAierken <g.aierken@gmail.com>
2020-07-16 22:13:47 -0400

    added react fragment

--- src/Components/Hackathon.js
+++ src/Components/Hackathon.js
@@ -2,8 +2,10 @@ import React from 'react'
 import {  Container } from 'semantic-ui-react'
 export default function Hackathon() {
     return (
-        <div>
-           <Container>Hackathons</Container>
-        </div>
+        <React.Fragment>
+            <Container>Hackathons</Container>
+        </React.Fragment>
+           
+        
     )
 }
